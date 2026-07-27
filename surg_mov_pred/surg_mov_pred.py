698cf8cc8b9e17d83221d683ce5530b84826ed27
Raphael Barret <raphaelnathan.barret@gmail.com>
2026-07-27 08:02:49 -0400

    generalisation of the data base

--- surg_mov_pred/surg_mov_pred.py
+++ surg_mov_pred/surg_mov_pred.py
@@ -5,8 +5,8 @@ from .src.surg_mov_pred_logic import Surg_mov_pred as smp
 class SurgMovPredTool(Tool):
     name = "surg_mov_pred"
     arguments = {
-        "model": ArgSpec(type="zip_file", required=True, description="Model package: a zip archive containing one or more stacking_package.pkl files"),
-        "input": ArgSpec(type="zip_file", required=True, description="Input data: a zip archive of a folder containing CSV / XLSX / ODS files"),
+        "model": ArgSpec(type="zip_file", required=True, description="Model package: a zip archive containing one or more stacking_package.pkl files", server_selectable="model"),
+        "input": ArgSpec(type="zip_file", required=True, description="Input data: a zip archive of a folder containing CSV / XLSX / ODS files", server_selectable="testfile"),
     }
     output_kind = "file"
 
